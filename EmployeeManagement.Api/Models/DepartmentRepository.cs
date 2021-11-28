8b8132cdc3d2e08e2070056aba477ed32d9c320b
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2021-11-28 14:08:43 +0100

    Set up repository pattern

---
+++ EmployeeManagement.Api/Models/DepartmentRepository.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public DepartmentRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public Department GetDepartment(int departmentId)
+        {
+            return _appDbContext.Departments.FirstOrDefault(d => d.DepartmentId == departmentId);
+        }
+
+        public IEnumerable<Department> GetDepartments()
+        {
+            return _appDbContext.Departments.ToList();
+        }
+    }
+}
